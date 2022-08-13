d284fe5b0fe3342e7514a7be4bac76fde1e3947d
Carlos Joco Gumanay <jocogum@DESKTOP-15283M7.localdomain>
2022-08-13 12:37:01 +0800

    add articles views and controllers

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,8 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  get '/articles' => 'articles#index'
+  get '/articles/new' => 'articles#new', as: 'new_article'
+  post '/articles' => 'articles#create', as: 'create_article'
+  
 end
