7e22e080a6ca6e5ae6b43fb08f99e5d614a33300
Carlos Joco Gumanay <jocogum@DESKTOP-15283M7.localdomain>
2022-08-15 20:50:44 +0800

    add edit, update, delete functionality

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -18,15 +18,19 @@ class ArticlesController < ApplicationController
   end
 
   def edit
-
+    @article = Article.find(params[:id])
   end
 
   def update
-
+    @article = Article.find(params[:id])
+    @article.update(name: params[:article][:name], body: params[:article][:body])
+    redirect_to articles_path
   end
 
   def delete
-
+    @article = Article.find(params[:id])
+    @article.destroy
+    redirect_to articles_path
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,9 @@ Rails.application.routes.draw do
   get '/articles' => 'articles#index'
   get '/articles/new' => 'articles#new', as: 'new_article'
   post '/articles' => 'articles#create', as: 'create_article'
-  
+
+  get '/articles/edit/:id' => 'articles#edit', as: 'edit_article'
+  patch '/articles/:id' => 'articles#update', as: 'update_article'
+  delete '/articles/:id' => 'articles#delete', as: 'delete_article'
+
 end
